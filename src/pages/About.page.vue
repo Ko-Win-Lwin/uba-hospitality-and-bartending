01ce4483bc7a88703d19b19229a6e4e3af8cb729
Ko-Win-Lwin <winlwin.mce@gmail.com>
2024-08-21 16:59:57 +0630

    about-page

--- src/pages/About.page.vue
+++ src/pages/About.page.vue
@@ -1,11 +1,116 @@
 <template>
-    <div>About</div>
+  <!-- About Section -->
+  <section id="about" class="about section light-background">
+    <Title title="About"></Title>
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+      <div class="row align-items-xl-center gx-5">
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+          <div class="icon-box">
+            <i class="bi bi-buildings"></i>
+            <h3>Eius provident</h3>
+            <p>
+              Magni repellendus vel ullam hic officia accusantium ipsa dolor
+              omnis dolor voluptatem
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-6 content">
+          <h2>Hospitality Management မှာအခွင့်အလမ်းရချင်သူတိုင်းအတွက် UBA</h2>
+          <p class="lh-lg">
+            ၂၀၁၉ ကနေ ယနေ့တိုင်ဖွင့်လှစ်လာခဲ့ပြီး ၅ နှစ်သက်တမ်းရှိပြီဖြစ်တဲ့ UBA
+            က ပြည်တွင်းမှာသာမက ပြည်ပမှာပါ ကျောင်းသူ/ သားပေါင်း ၂၀၀၀ ကျော်အထိ
+            မွေးထုတ်ပေးခဲ့ပြီဖြစ်ပါတယ်။ Hospitality (Hotel) နဲ့ Bar
+            သင်တန်းကျောင်းဖြစ်တဲ့ UBA မှာ
+          </p>
+          <ul>
+            <li>Housekeeping Course</li>
+            <li>Food & Beverage Service Course</li>
+            <li>Bartending Course</li>
+            <li>nglish for Hospitality Course</li>
+          </ul>
+          <p class="lh-lg">
+            တွေကို လစဉ်လတိုင်း ဖွင့်လှစ်ပေးထားပါတယ်။ Hotel လုပ်ငန်းမှာ
+            Professional career တစ်ခုအထိ တက်လှမ်းနိုင်ဖို့ တစ်လတည်းနဲ့ စာတွေ့၊
+            လက်တွေ့၊ On job training, Internship အပြင် Interview
+            အတွက်ပါစီစဉ်သင်ကြားပေနေတဲ့ UBA မှာ ပြည်တွင်းနဲ့
+            ပြည်ပနိုင်ငံများဖြစ်တဲ့ UAE, Qatar, Kuwait, Singapore နိုင်ငံများရှိ
+            ဟိုတယ်၊ စားသောက်ဆိုင်များနဲ့တိုက်ရိုက်အလုပ်အကိုင်ရရှိဖို့အတွက်လည်း
+            အားတက်သရောချိတ်ဆက်ပေးပါတယ်နော်။
+          </p>
+
+          <p class="lh-lg">
+            နယ်မှသင်တန်းတက်ချင်တဲ့သူတွေအတွက် လည်း အဆောင်ကောင်းများရှိပါတယ်။ 💰
+            သင်တန်းကြေးစရံများကြိုသွင်းရန်အတွက်လည်း
+          </p>
+          <ul>
+            <li>KBZ pay</li>
+            <li>Wave pay</li>
+            <li>UAB pay</li>
+            <li>AYA pay</li>
+          </ul>
+          <p class="lh-lg">စတဲ့ Payment တွေ လက်ခံပေးပါတယ်။</p>
+          <p class="lh-lg">
+            အခုလာမယ့်ဧပြီလဟာဆိုရင် ပြည်ပကိုထွက်ဖို့
+            ပြင်ဆင်ချိန်အကောင်းဆုံးဖြစ်တာမို့
+            နိုင်ငံရပ်ခြားမှာငွေရှာပြီးကြိုးစားချင်သူလူငယ်များကို UBA မှ
+            နွေးထွေးစွာကြိုဆိုလိုက်ပါတယ်။
+          </p>
+          <p class="fw-bold">ဆက်သွယ်ရန်</p>
+          <div class="d-flex justify-content-between align-items-center gap-2">
+            <div class="d-flex gap-3">☎️<span class="fw-semibold">09 740 828 924</span></div>
+            <div class="d-flex gap-3">☎️<span class="fw-semibold">09 760 654 828</span></div>
+            <div class="d-flex gap-3">☎️<span class="fw-semibold">09 407 654 449</span></div>
+          </div>
+
+          <p class="lh-lg mt-4 fw-semibold">
+            လိပ်စာ_အမှတ် ၂၄၉၊ မြေညီထပ်၊ ဇမ္ဗူသီရိ၈လမ်း၊ ၆အနောက်ရပ်ကွက်၊
+            ရုပ်ရှင်ရုံမှတ်တိုင်အနီး၊
+          </p>
+          <p class="fw-semibold">သာကေတမြို့နယ်၊ ရန်ကုန်မြို့။</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /About Section -->
+  <!-- Hospitality Management မှာအခွင့်အလမ်းရချင်သူတိုင်းအတွက် UBA
+
+၂၀၁၉ ကနေ ယနေ့တိုင်ဖွင့်လှစ်လာခဲ့ပြီး ၅ နှစ်သက်တမ်းရှိပြီဖြစ်တဲ့ UBA က ပြည်တွင်းမှာသာမက ပြည်ပမှာပါ ကျောင်းသူ/ သားပေါင်း ၂၀၀၀ ကျော်အထိ မွေးထုတ်ပေးခဲ့ပြီဖြစ်ပါတယ်။ 
+
+Hospitality (Hotel) နဲ့ Bar သင်တန်းကျောင်းဖြစ်တဲ့ UBA မှာ 
+▫️Housekeeping Course
+▫️Food & Beverage Service Course
+▫️Food & Beverage Bartending Course အပြင် 
+▫️English for Hospitality Course တွေကိုပါ လစဉ်လတိုင်း ဖွင့်လှစ်ပေးထားပါတယ်။ 
+
+Hotel လုပ်ငန်းမှာ Professional career တစ်ခုအထိ တက်လှမ်းနိုင်ဖို့ တစ်လတည်းနဲ့ စာတွေ့၊ လက်တွေ့၊ On job training, Internship အပြင် Interview အတွက်ပါစီစဉ်သင်ကြားပေနေတဲ့ UBA မှာ 
+
+ပြည်တွင်းနဲ့ ပြည်ပနိုင်ငံများဖြစ်တဲ့ UAE, Qatar, Kuwait, Singapore နိုင်ငံများရှိ ဟိုတယ်၊ စားသောက်ဆိုင်များနဲ့တိုက်ရိုက်အလုပ်အကိုင်ရရှိဖို့အတွက်လည်း အားတက်သရောချိတ်ဆက်ပေးပါတယ်နော်။ 
+
+▫️ နယ်မှသင်တန်းတက်ချင်တဲ့သူတွေအတွက် လည်း အဆောင်ကောင်းများရှိပါတယ်။ 
+
+💰 သင်တန်းကြေးစရံများကြိုသွင်းရန်အတွက်လည်း (KBZ pay, Wave pay, UAB pay, AYA pay) စတဲ့ Payment တွေ လက်ခံပေးပါတယ်။
+
+☀️ အခုလာမယ့်ဧပြီလဟာဆိုရင်
+ပြည်ပကိုထွက်ဖို့ ပြင်ဆင်ချိန်အကောင်းဆုံးဖြစ်တာမို့ နိုင်ငံရပ်ခြားမှာငွေရှာပြီးကြိုးစားချင်သူလူငယ်များကို UBA မှ နွေးထွေးစွာကြိုဆိုလိုက်ပါတယ်။ 
+
+ဆက်သွယ်ရန် 
+
+☎️09 740 828 924
+☎️09 760 654 828
+☎️09 407 654 449
+
+လိပ်စာ_အမှတ် ၂၄၉၊ မြေညီထပ်၊ ဇမ္ဗူသီရိ၈လမ်း၊ ၆အနောက်ရပ်ကွက်၊ ရုပ်ရှင်ရုံမှတ်တိုင်အနီး၊ သာကေတမြို့နယ်၊ ရန်ကုန်မြို့။
+
+#Bartender
+#Waiterwaitress
+#Housekeeping #HospitalityCourse #BeverageBartendingCourse #HousekeepingCourse #FandBServiceCourse
+#English #OJT #Overseajobs #Inteview #Management
+#UBA -->
 </template>
 
 <script setup>
-
+import Title from "../components/Title.component.vue";
 </script>
 
 <style>
-
 </style>
