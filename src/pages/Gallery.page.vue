6eeffc278ba6685d91013097b3251127e5474465
Ko-Win-Lwin <winlwin.mce@gmail.com>
2024-08-21 19:48:50 +0630

    gallery: redesign image

--- src/pages/Gallery.page.vue
+++ src/pages/Gallery.page.vue
@@ -1,10 +1,10 @@
 <template>
   <Title title="Gallery"></Title>
-  <section id="portfolio" class="portfolio section">
+  <section id="" class="">
     <div class="container">
       <!-- Gallery -->
       <div class="row">
-        <div v-for="img in [2,3, 1,4,5,6,7]" :key="img" class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        <div v-for="img in [1,4,5,2,3,6]" :key="img" class="col-lg-4 col-md-12 mb-4 mb-lg-0">
           <img
             :src="getImagePath(img)"
             class="w-100 shadow-1-strong rounded mb-4"
@@ -19,7 +19,6 @@
 
 <script setup>
 import Title from "../components/Title.component.vue";
-
 
 const getImagePath = (image) => {
     return `src/assets/img/gallery/gallery-${image}.jpg`;
