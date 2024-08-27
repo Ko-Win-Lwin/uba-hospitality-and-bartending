990c573f9b4c032063c7199e444528bc7da6ce10
Ko-Win-Lwin <winlwin.mce@gmail.com>
2024-08-27 16:15:29 +0630

    home-content

---
+++ src/components/AboutContent.component.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="my-5 features" style="height: fit-content;">
+        <div class="py-4">
+            <div class="container section-title" data-aos="fade-up">
+                <Title :title="title"></Title>
+            </div>
+
+            <div class="container">
+                <div class="row align-items-stretch justify-content-between gap-4 features-item">
+                    <div class="col-lg-5 d-flex justify-content-center flex-column" data-aos="fade-up">
+                        <p class="lh-lg fs-5">
+                            <slot></slot>
+                        </p>
+                    </div>
+                
+                    <div class="col-lg-6 d-flex align-items-center features-img-bg" data-aos="zoom-out">
+                        <img src="../assets/img/blog/blog-1.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+<!-- 
+        <div class="col-lg-6 order-1  order-lg-2 d-flex align-items-center" data-aos="zoom-out"
+                    data-aos-delay="100">
+                    <div class="image-stack">
+                        <img src="../assets/img/blog/blog-1.jpg" alt="" class="stack-front">
+                        <img src="../assets/img/blog/blog-1.jpg" alt="" class="stack-back">
+                    </div>
+                </div> -->
+    </div>
+</template>
+
+<script setup>
+import Title from './Title.component.vue';
+defineProps({
+    title: String
+})
+</script>
--- src/pages/Home.page.vue
+++ src/pages/Home.page.vue
@@ -2,18 +2,18 @@
   <main class="main">
     <!-- Hero Section -->
     <section id="hero" class="hero section dark-background">
-      <img :src="images[currentImageIndex]" alt="" data-aos="fade-in" class="img-fluid w-100"/>
+      <img :src="images[currentImageIndex]" alt="" data-aos="fade-in" class="img-fluid w-100" />
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
 
             <h2 data-aos="fade-up" data-aos-delay="100" class="lh-lg fs-2">
               <div>လက်တွေ့မှာ ဘာစလုပ်ရမှန်းမသိသေးရင် ကျွန်တော်တို့ရဲ့ </div>
-              <di>UBA - United Bartender & Hospitality Academy ကို</di> 
+              <di>UBA - United Bartender & Hospitality Academy ကို</di>
               <div>လှမ်းခဲ့ဖို့ ဖိတ်ခေါ်ပါရစေလား</div>
             </h2>
-            <RouterLink class="btn btn-outline-orange mt-4  px-lg-4 py-lg-2 fs-5 fw-semibold" to="/contact"
-              >Contact</RouterLink>
+            <RouterLink class="btn btn-outline-orange mt-4  px-lg-4 py-lg-2 fs-5 fw-semibold" to="/contact">Contact
+            </RouterLink>
           </div>
         </div>
       </div>
@@ -21,8 +21,44 @@
     <!-- /Hero Section -->
   </main>
 
+  <div id="vision" class="bg-light py-5">
+    <AboutContent title="Vision">
+      Our vision is to empower individuals born and raised in Myanmar (Burma) by providing
+      accessible, up-to-date education in the dynamic hospitality industry. We believe that a career in
+      hospitality offers young people a pathway to stable income and abundant opportunities, both
+      locally and abroad. UBA is committed to making this vision a reality by equipping our students
+      with the skills and knowledge necessary to succeed in the global hospitality sector.
+    </AboutContent>
+  </div>
+
+  <section id="success" class="py-5">
+    <AboutContent title="Our Success">
+      UBA's journey since its inception in 2019 has been marked by significant achievements. As of
+      August 2024, we have successfully conducted 67 batches, enrolling students on a monthly basis.
+      Over 4,600 students have completed our programs, each equipped with the skills and knowledge
+      to excel in the hospitality industry. Our thousand of students have not only secured promising
+      opportunities in local restaurants and hotels but have also made their mark on the international
+      stage, contributing to UBA's reputation as a leader in hospitality education.
+    </AboutContent>
+  </section>
+
+  <section id="guarantee" class="bg-light my-0">
+    <AboutContent title="Our Guarantee">
+      At UBA, we are committed to providing young people and the new generation with
+      comprehensive education that blends both theoretical and practical training, aligned with
+      international hospitality standards. Our curriculum is designed to equip students with the skills
+      necessary to thrive in five-star hotels and restaurants worldwide.
+      Upon completion of their courses, our students are offered internships in local restaurants and
+      hotels, ensuring they gain valuable real-world experience. Additionally, UBA has established a
+      strong network that facilitates job placements in foreign countries, including Thailand, Malaysia,
+      Singapore, Taiwan, the Maldives, UAE, Qatar, Bahrain, Kuwait, and more. This global reach
+      ensures that our graduates have access to a wealth of career opportunities beyond Myanmar.
+    </AboutContent>
+  </section>
+
   <!-- Our Courses -->
-  <section>
+  <!--
+  <section class="mb-4">
     <Title title="Our Courses"></Title>
     <CourseList></CourseList>
     <div class="d-grid place-items-center mt-4">
@@ -34,13 +70,14 @@
         More Courses
       </button>
     </div>
-  </section>
+  </section> 
+  -->
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
-import CourseList from "../components/Courses/CourseList.component.vue";
-import Title from "../components/Title.component.vue";
+
+import AboutContent from "../components/AboutContent.component.vue";
 import { onBeforeUnmount, onMounted, ref } from "vue";
 
 const router = useRouter();
